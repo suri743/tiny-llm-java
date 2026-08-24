4f800065bd2f37639c76590fc07e79f07f3664a3
Surish Ayya <suresh.ayya@sap.com>
2026-08-24 21:28:56 +0530

    Fix EBS volume size to 30GB

--- infra/main.tf
+++ infra/main.tf
@@ -142,7 +142,7 @@ resource "aws_instance" "training" {
   vpc_security_group_ids = [aws_security_group.training.id]
 
   root_block_device {
-    volume_size = 20
+    volume_size = 30
     volume_type = "gp3"
   }
 
